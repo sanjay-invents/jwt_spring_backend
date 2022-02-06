8a9f81190c244d3d4cf88efd43c103c220116d30
sanjay-invents <sanjay.invents@gmail.com>
2022-02-06 14:23:11 +0545

    Section 3 Completed

---
+++ src/main/java/com/shoryukane/supportportal/resource/UserResource.java
@@ -0,0 +1,17 @@
+package com.shoryukane.supportportal.resource;
+
+import com.shoryukane.supportportal.Domain.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserResource {
+
+    @GetMapping("/home")
+    public String showUser() {
+        return "application works";
+    }
+
+}
