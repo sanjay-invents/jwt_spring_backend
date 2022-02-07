6bfd34a240c92de588c7bd494e821e21dd5c8041
sanjay-invents <sanjay.invents@gmail.com>
2022-02-07 11:14:06 +0545

    Section 10 Completed

--- src/main/java/com/shoryukane/supportportal/service/UserService.java
+++ src/main/java/com/shoryukane/supportportal/service/UserService.java
@@ -2,10 +2,13 @@ package com.shoryukane.supportportal.service;
 
 import com.shoryukane.supportportal.domain.User;
 import com.shoryukane.supportportal.exception.domain.EmailExistException;
+import com.shoryukane.supportportal.exception.domain.EmailNotFoundException;
 import com.shoryukane.supportportal.exception.domain.UserNotFoundException;
 import com.shoryukane.supportportal.exception.domain.UsernameExistException;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.mail.MessagingException;
+import java.io.IOException;
 import java.util.List;
 
 public interface UserService {
@@ -16,4 +19,14 @@ public interface UserService {
     User findUserByUsername(String username);
 
     User findUserByEmail(String email);
+
+    User addNewUser(String firstName, String lastName, String username, String email, String role, boolean isNonLocked, boolean isActive, MultipartFile profileImage) throws UserNotFoundException, EmailExistException, UsernameExistException, IOException;
+
+    User updateUser(String currentUsername, String newFirstName, String newLastName, String newUsername, String newEmail, String role, boolean isNonLocked, boolean isActive, MultipartFile profileImage) throws UserNotFoundException, EmailExistException, UsernameExistException, IOException;
+
+    void deleteUser(long id);
+
+    void resetPassword(String email) throws MessagingException, EmailNotFoundException;
+
+    User updateProfileImage(String username, MultipartFile profileImage) throws UserNotFoundException, EmailExistException, UsernameExistException, IOException;
 }
